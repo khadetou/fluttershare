34ab8637b065193d8c595119f9daa3fe1f6f2ed4
khadetou <khadetou96@gmail.com>
2022-02-28 19:04:35 +0000

    progress

---
+++ lib/widgets/progress.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+circurlarProgress() {
+  return Container(
+    alignment: Alignment.center,
+    padding: const EdgeInsets.only(top: 10.0),
+    child: const CircularProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(Colors.purple),
+    ),
+  );
+}
+
+linearProgress() {
+  return Container(
+    padding: const EdgeInsets.only(bottom: 10.0),
+    child: const LinearProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(Colors.purple),
+    ),
+  );
+}
