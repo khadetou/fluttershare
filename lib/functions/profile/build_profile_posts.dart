dfe2085719a2643343fd89246f7bc6e827d996a1
khadetou <khadetou96@gmail.com>
2022-03-12 18:00:37 +0000

    building no content for profile page

--- lib/functions/profile/build_profile_posts.dart
+++ lib/functions/profile/build_profile_posts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fluttershare/widgets/post_tile.dart';
 
 import '../../widgets/progress.dart';
@@ -10,6 +11,27 @@ buildProfilePosts({
 }) {
   if (isLoading) {
     return circularProgress();
+  } else if (posts.isEmpty) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset(
+          'assets/images/no_content.svg',
+          height: 260.0,
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 20.0),
+          child: Text(
+            "No Posts",
+            style: TextStyle(
+              color: Colors.redAccent,
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
   } else if (postOrientation == "grid") {
     List<GridTile> gridTiles = [];
     posts.forEach((post) {
